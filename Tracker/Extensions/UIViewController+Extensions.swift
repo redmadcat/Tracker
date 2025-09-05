11dea37bd8ab4bceb698e5adeb8adaaa01cd0db1
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-09-05 12:09:18 +0300

    add: view controller ext
    Add UIViewControllerRepresentable extension.

---
+++ Tracker/Extensions/UIViewController+Extensions.swift
@@ -0,0 +1,34 @@
+//
+//  UIViewController+Extensions.swift
+//  Tracker
+//
+//  Created by Roman Yaschenkov on 05.09.2025.
+//
+
+import UIKit
+
+#if DEBUG
+import SwiftUI
+
+@available(iOS 13, *)
+extension UIViewController {
+    static var devices = ["iPnone SE", "iPhone 11 Pro Max"]
+    
+    private struct Preview: UIViewControllerRepresentable {
+        // this variable is used for injecting the current view controller
+        let viewController: UIViewController
+
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+
+    func toPreview() -> some View {
+        // inject self (the current view controller) for the preview
+        Preview(viewController: self)
+    }
+}
+#endif
