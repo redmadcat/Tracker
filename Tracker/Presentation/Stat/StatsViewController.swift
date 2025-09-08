d767cd5902b3942e01814947eb31bc52cdf11695
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-09-08 12:17:45 +0300

    add: stats controller impl
    Add StatsViewController implementation.

--- Tracker/Presentation/Stat/StatsViewController.swift
+++ Tracker/Presentation/Stat/StatsViewController.swift
@@ -10,4 +10,24 @@ import Foundation
 
 final class StatsViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .ypWhite
+    }
 }
+
+#if DEBUG
+import SwiftUI
+
+@available(iOS 13, *)
+struct StatsViewControllerPreview: PreviewProvider {
+    static var previews: some View {
+        ForEach(UIViewController.devices, id: \.self) { deviceName in
+            StatsViewController().toPreview()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
+#endif
