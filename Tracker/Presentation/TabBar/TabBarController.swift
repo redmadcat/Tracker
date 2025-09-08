f592098e6e93501db01b2f617b774e80b1ed7505
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-09-08 12:15:17 +0300

    add: tab bar controller impl
    Add TabBarController implementation.

--- Tracker/Presentation/TabBar/TabBarController.swift
+++ Tracker/Presentation/TabBar/TabBarController.swift
@@ -11,6 +11,49 @@ final class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+                
+        configureTabBarItems()
+        configureTabBar()
+    }
+    
+    private func configureTabBarItems() {
+        let trackersViewController = TrackersViewController()
+        let statsViewController = StatsViewController()
+        
+        trackersViewController.tabBarItem = UITabBarItem(
+            title: "trackers",
+            image: UIImage(named: "StatsActive"),
+            selectedImage: nil
+        )
+        
+        statsViewController.tabBarItem = UITabBarItem(
+            title: "stats",
+            image: UIImage(named: "StatsNoActive"),
+            selectedImage: nil
+        )
+        
+        viewControllers = [trackersViewController, statsViewController]
+    }
+    
+    private func configureTabBar() {
+        tabBar.backgroundColor = .ypWhite
+        tabBar.tintColor = .ypBlue
+        tabBar.unselectedItemTintColor = .ypGray
+        tabBar.addBorder(to: .top, width: 0.5, color: .ypGray)
     }
 }
+
+#if DEBUG
+import SwiftUI
+
+@available(iOS 13, *)
+struct TabBarControllerPreview: PreviewProvider {
+    static var previews: some View {
+        ForEach(UIViewController.devices, id: \.self) { deviceName in
+            TabBarController().toPreview()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
+#endif
