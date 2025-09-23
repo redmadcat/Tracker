1db97696d5456539494ed6eca87ad4ba6e29e1ea
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-09-23 15:49:13 +0300

    fix: tracker schedule vc layout impl
    Fix TrackerScheduleViewController layout implementation.

--- Tracker/Presentation/Tracker/TrackerScheduleViewController.swift
+++ Tracker/Presentation/Tracker/TrackerScheduleViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Foundation
 
 final class TrackerScheduleViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     // MARK: - Definition
@@ -13,13 +14,12 @@ final class TrackerScheduleViewController: UIViewController, UITableViewDataSour
         var tableView = UITableView.init(frame: .zero, style: UITableView.Style.plain)
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.isScrollEnabled = false
-//        tableView.register(TrackerNameCell.self, forCellReuseIdentifier: TrackerNameCell.reuseIdentifier)
+        tableView.register(TrackerWeekdayCell.self, forCellReuseIdentifier: TrackerWeekdayCell.reuseIdentifier)
         tableView.separatorStyle = .none
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
-    
+        
     private let headerLabel: UILabel = {
         let headerLabel = UILabel(
             text: "Расписание",
@@ -30,6 +30,19 @@ final class TrackerScheduleViewController: UIViewController, UITableViewDataSour
         return headerLabel
     }()
     
+    private var createButton: UIButton = {
+        let createButton = UIButton()
+        createButton.setTitle("Готово", for: .normal)
+        createButton.setTitleColor(.ypWhite, for: .normal)
+        createButton.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        createButton.backgroundColor = .ypBlack
+        createButton.layer.cornerRadius = 16
+        createButton.translatesAutoresizingMaskIntoConstraints = false
+        return createButton
+    }()
+    
+    private var selectedWeekday = [Int]()
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,15 +52,15 @@ final class TrackerScheduleViewController: UIViewController, UITableViewDataSour
     
     // MARK: - UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 7
+        return Calendar.weekday.count
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        return TrackerWeekdayCell(style: .default, reuseIdentifier: TrackerWeekdayCell.reuseIdentifier, index: indexPath.row)
     }
     
     // MARK: - UITableViewDelegate
@@ -58,16 +71,28 @@ final class TrackerScheduleViewController: UIViewController, UITableViewDataSour
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
     }
-    
+        
     // MARK: - Private func
     private func configureLayout() {
         view.addSubview(headerLabel)
+        view.addSubview(tableView)
+        view.addSubview(createButton)
         
         NSLayoutConstraint.activate([
             headerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 34),
             headerLabel.widthAnchor.constraint(equalToConstant: 133),
             headerLabel.heightAnchor.constraint(equalToConstant: 22),
             headerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 38),
+            tableView.bottomAnchor.constraint(equalTo: createButton.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            createButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            createButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            createButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            createButton.heightAnchor.constraint(equalToConstant: 60),
         ])
         
         view.backgroundColor = .ypWhite
