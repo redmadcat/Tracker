317ed93e03f1f2d6446d2c995064216df656e374
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-09-29 14:12:44 +0300

    fix: tracker text field padding impl
    Fix TrackerTextField padding implementation.

--- Tracker/Presentation/Tracker/TrackerTextField.swift
+++ Tracker/Presentation/Tracker/TrackerTextField.swift
@@ -8,24 +8,22 @@
 import UIKit
 
 final class TrackerTextField: UITextField {
+    private let padding = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 41)
 
     override func textRect(forBounds bounds: CGRect) -> CGRect {
-        let rect = super.textRect(forBounds: bounds)
-        return rect.offsetBy(dx: 16, dy: 0)
+        return bounds.inset(by: padding)
+    }
+
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
     }
     
     override func clearButtonRect(forBounds bounds: CGRect) -> CGRect {
         let rect = super.clearButtonRect(forBounds: bounds)
         return rect.offsetBy(dx: -8, dy: 0)
-    }
-    
-    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
-        let rect = super.textRect(forBounds: bounds)
-        return rect.offsetBy(dx: 16, dy: 0)
-    }
-
-    override func editingRect(forBounds bounds: CGRect) -> CGRect {
-        let rect = super.textRect(forBounds: bounds)
-        return rect.offsetBy(dx: 16, dy: 0)
     }
 }
