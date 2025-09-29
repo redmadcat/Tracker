d0613e365b0786c8d3ef2952403668b1e35ebcef
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-09-29 14:17:51 +0300

    add: is hidden init param
    Add isHidden initializer parameter.

--- Tracker/Extensions/UILabel+Extensions.swift
+++ Tracker/Extensions/UILabel+Extensions.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 extension UILabel {
-    convenience init(text: String, textColor: UIColor, font: UIFont = .systemFont(ofSize: 13), textAlighment: NSTextAlignment = .left) {
+    convenience init(text: String, textColor: UIColor, font: UIFont = .systemFont(ofSize: 13), textAlighment: NSTextAlignment = .left,
+                     isHidden: Bool = false) {
         self.init()
         self.translatesAutoresizingMaskIntoConstraints = false
         self.text = text
         self.textColor = textColor
         self.font = font
         self.textAlignment = textAlighment
+        self.isHidden = isHidden
     }
 }
