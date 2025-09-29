3a43dd23bbb1126ae19b947d8527197dadfb5f37
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-09-29 14:21:13 +0300

    fix: add border impl
    Fix addBorder implementation to prevent animation issue.

--- Tracker/Presentation/Tracker/TrackerCategoryCell.swift
+++ Tracker/Presentation/Tracker/TrackerCategoryCell.swift
@@ -21,6 +21,7 @@ final class TrackerCategoryCell: UITableViewCell {
         detailTextLabel?.font = .systemFont(ofSize: 17)
         detailTextLabel?.textColor = .ypGray
         contentView.superview?.backgroundColor = .ypBackground
+        addBorder(to: .bottom, width: 0.5, color: .ypGray, inset: 20)
     }
     
     required init?(coder: NSCoder) {
@@ -30,6 +31,5 @@ final class TrackerCategoryCell: UITableViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         roundCorners(corners: [.topLeft, .topRight], radius: 16)
-        addBorder(to: .bottom, width: 0.5, color: .ypGray, inset: 20)
     }
 }
