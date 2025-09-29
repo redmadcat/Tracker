ea6d2831c6a1e1a26fa857cfa208409b58ecdab4
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-09-29 14:19:32 +0300

    add: warning limit label impl
    Add: warningLimitLabel/accessor member implementation.

--- Tracker/Presentation/Tracker/TrackerStubCell.swift
+++ Tracker/Presentation/Tracker/TrackerStubCell.swift
@@ -8,13 +8,35 @@
 import UIKit
 
 final class TrackerStubCell: UITableViewCell {
+    // MARK: - Definition
+    private let warningLimitLabel: UILabel = {
+        return UILabel(
+            text: "Ограничение 38 символов",
+            textColor: .ypRed,
+            font:.systemFont(ofSize: 17, weight: .regular),
+            textAlighment: .center,
+            isHidden: true)
+    }()
+    
     // MARK: - Lifecycle
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String? = TrackerStubCell.reuseIdentifier) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         selectionStyle = .none
+        
+        contentView.addSubview(warningLimitLabel)
+        
+        NSLayoutConstraint.activate([
+            warningLimitLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            warningLimitLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            warningLimitLabel.heightAnchor.constraint(equalToConstant: 22),
+        ])
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+    }
+    
+    func showWarning(_ value: Bool) {
+        warningLimitLabel.isHidden = !value
     }
 }
