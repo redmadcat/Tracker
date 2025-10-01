5cdcf4d20143626d56ceac8747c80a2aba0b1613
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-10-01 10:23:15 +0300

    feat: add weekday symbol wrapper impl
    Feat, add weekday symbol wrapper implementation for simplified usage.

--- Tracker/Extensions/Calendar+Extensions.swift
+++ Tracker/Extensions/Calendar+Extensions.swift
@@ -8,21 +8,26 @@
 import Foundation
 
 extension Calendar {
-    private static let calendar: Calendar = {
+    private static let local: Calendar = {
         var calendar = Calendar(identifier: .iso8601)
         calendar.locale = Locale(identifier: "ru_RU")
         return calendar
     }()
         
-    static let weekday: [String] = {
-        var weekdaySymbols = calendar.weekdaySymbols
-        weekdaySymbols.append(weekdaySymbols.remove(at: weekdaySymbols.startIndex))
-        return weekdaySymbols
-    }()
+    static func weekdaySymbol(at index: Int) -> String? {
+        return local.weekdaySymbols[safe: index]?.firstUppercased
+    }
     
-    static let shortWeekday: [String] = {
-        var weekdaySymbols = calendar.shortWeekdaySymbols
-        weekdaySymbols.append(weekdaySymbols.remove(at: weekdaySymbols.startIndex))
-        return weekdaySymbols
+    static func shortWeedaySymbols(at range: [Int]) -> String {
+        return local.shortWeekdaySymbols.elements(at: range).joined(separator: ", ")
+    }
+                        
+    static func weekdayNumber(for date: Date) -> Int? {
+        let weekday = local.component(.weekday, from: date)
+        return local.weekdaySymbols.firstIndex(of: local.weekdaySymbols[weekday - 1])
+    }
+    
+    static let weekdayCount: Int = {
+        return local.shortWeekdaySymbols.count
     }()
 }
