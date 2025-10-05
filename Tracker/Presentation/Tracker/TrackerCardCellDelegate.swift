8b0fd44945ac0a45d949c49910eb96bebda467e7
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-10-05 14:59:13 +0300

    refactor: simplify tracker card cell delegate impl
    Refactor, simplify TrackerCardCellDelegate implementation.

--- Tracker/Presentation/Tracker/TrackerCardCellDelegate.swift
+++ Tracker/Presentation/Tracker/TrackerCardCellDelegate.swift
@@ -5,6 +5,8 @@
 //  Created by Roman Yaschenkov on 01.10.2025.
 //
 
+import Foundation
+
 protocol TrackerCardCellDelegate: AnyObject {
-    func trackerCardCellDidTapComplete(_ cell: TrackerCardCell)
+    func setCompletionTo(completion: Bool, with id: UUID, at indexPath: IndexPath)
 }
