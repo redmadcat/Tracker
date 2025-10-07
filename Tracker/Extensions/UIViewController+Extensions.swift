8942dbc36f3ffce7addc502becec1044efc53bb2
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-10-07 08:55:36 +0300

    refactor: remove deprecated members
    Refactor, remove deprecated members.

--- Tracker/Extensions/UIViewController+Extensions.swift
+++ Tracker/Extensions/UIViewController+Extensions.swift
@@ -7,30 +7,7 @@
 
 import UIKit
 
-#if DEBUG
-import SwiftUI
-
-@available(iOS 13, *)
 extension UIViewController {
-    static var devices = ["iPnone SE", "iPhone 11 Pro Max"]
-    
-    private struct Preview: UIViewControllerRepresentable {
-        // this variable is used for injecting the current view controller
-        let viewController: UIViewController
-
-        func makeUIViewController(context: Context) -> UIViewController {
-            return viewController
-        }
-
-        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-        }
-    }
-
-    func toPreview() -> some View {
-        // inject self (the current view controller) for the preview
-        Preview(viewController: self)
-    }
-    
     func hideKeyboardWhenTappedAround() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         tap.cancelsTouchesInView = false
@@ -41,4 +18,3 @@ extension UIViewController {
         view.endEditing(true)
     }
 }
-#endif
