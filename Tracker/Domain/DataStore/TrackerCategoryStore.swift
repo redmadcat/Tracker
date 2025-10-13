59c5f4157c613fa667f2a12f05bb46bd49161184
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-10-13 14:38:23 +0300

    fix: add tracker category impl
    Fix addTrackerCategory member implementation.

--- Tracker/Domain/DataStore/TrackerCategoryStore.swift
+++ Tracker/Domain/DataStore/TrackerCategoryStore.swift
@@ -19,9 +19,20 @@ final class TrackerCategoryStore {
         self.context = context
     }
     
-    func addTrackerCategory(_ trackerCategory: TrackerCategory) throws {
-        let tracker = TrackerCategoryCoreData(context: context)
-        tracker.header = trackerCategory.header
+    func addTrackerCategory(_ trackerCategory: TrackerCategory) throws -> TrackerCategoryCoreData {
+        if let existingCategory = findCategoryAt(header: trackerCategory.header) { return existingCategory }
+        let category = TrackerCategoryCoreData(context: context)
+        category.header = trackerCategory.header
         try context.save()
+        return category
+    }
+    
+    private func findCategoryAt(header: String) -> TrackerCategoryCoreData? {
+        let request = NSFetchRequest<TrackerCategoryCoreData>(entityName: "TrackerCategoryCoreData")
+        request.returnsObjectsAsFaults = false
+        request.predicate = NSPredicate(format: "%K == %@", #keyPath(TrackerCategoryCoreData.header), header)
+        
+        let category = try! context.fetch(request)
+        return category.count > 0 ? category.first : nil
     }
 }
