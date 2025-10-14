f496ff29e9032cdbb60dea1ceb858693db9337cf
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-10-14 09:42:51 +0300

    add: transform to tracker member impl
    Add transformToTracker member implementation.

--- Tracker/Domain/DataStore/TrackerStore.swift
+++ Tracker/Domain/DataStore/TrackerStore.swift
@@ -8,6 +8,10 @@
 import CoreData
 import UIKit
 
+private enum TrackerTransformError: Error {
+    case transformErrorInvalidData
+}
+
 final class TrackerStore {
     private let context: NSManagedObjectContext
     
@@ -21,15 +25,26 @@ final class TrackerStore {
     }
         
     func addTracker(_ tracker: Tracker, category: TrackerCategoryCoreData) throws {
-        let schedule = tracker.schedule.map { String($0) }.joined(separator: "")
-        
         let newTracker = TrackerCoreData(context: context)
         newTracker.id = tracker.id
         newTracker.name = tracker.name
         newTracker.emoji = tracker.emoji
         newTracker.color = tracker.color
-        newTracker.schedule = schedule
+        newTracker.schedule = tracker.schedule.map { String($0) }.joined(separator: "")
         newTracker.category = category
         try context.save()
+    }
+    
+    func transformToTracker(from coreDataObject: TrackerCoreData) throws -> Tracker {
+        guard let id = coreDataObject.id,
+              let name = coreDataObject.name,
+              let color = coreDataObject.color as? UIColor,
+              let emoji = coreDataObject.emoji,
+              let values = coreDataObject.schedule
+        else { throw TrackerTransformError.transformErrorInvalidData }
+        
+        let schedule = values.compactMap { $0.wholeNumberValue }
+        
+        return Tracker(id: id, name: name, color: color, emoji: emoji, schedule: schedule)
     }
 }
