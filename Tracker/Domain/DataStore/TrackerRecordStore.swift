83a59d69412a9b42f60aecf8c190f6b29b85e6c6
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-10-16 16:04:07 +0300

    add: tracker record store impl
    Add TrackerRecordStore implementation.

--- Tracker/Domain/DataStore/TrackerRecordStore.swift
+++ Tracker/Domain/DataStore/TrackerRecordStore.swift
@@ -26,5 +26,33 @@ final class TrackerRecordStore {
         try context.save()
     }
     
+    func delete(_ trackerRecord: TrackerRecord) throws {
+        guard let (from, to) = Calendar.range(for: trackerRecord.date) else { return }
+        let request = NSFetchRequest<TrackerRecordCoreData>(entityName: "TrackerRecordCoreData")
+        request.fetchLimit = 1
+        request.predicate = NSPredicate(format: "trackerId == %@ AND (date >= %@ AND date < %@)",
+                                        trackerRecord.trackerId as CVarArg, from as CVarArg, to as CVarArg)
+        
+        if let record = try! context.fetch(request).first {
+            context.delete(record)
+            try context.save()
+        }
+    }
     
+    func isCompleted(for trackerId: UUID, at date: Date) throws -> Bool? {
+        guard let (from, to) = Calendar.range(for: date) else { return nil }
+        let request = NSFetchRequest<TrackerRecordCoreData>(entityName: "TrackerRecordCoreData")
+        request.predicate = NSPredicate(format: "%K == %@ AND (date >= %@ AND date < %@)",
+            #keyPath(TrackerRecordCoreData.trackerId), trackerId as CVarArg,
+            from as CVarArg, to as CVarArg)
+
+        return try context.count(for: request) > 0 ? true : false
+    }
+    
+    func count(at trackerId: UUID) -> Int {
+        let request = NSFetchRequest<TrackerRecordCoreData>(entityName: "TrackerRecordCoreData")
+        request.predicate = NSPredicate(format: "%K == %@", #keyPath(TrackerRecordCoreData.trackerId), trackerId as CVarArg)
+        
+        return try! context.count(for: request)
+    }
 }
