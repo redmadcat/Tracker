283f11c06f2efc463bfb739c934bc606785e5f67
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-10-22 14:13:44 +0300

    add: tracker category vm decl
    Add TrackerCategoryViewModel declaration.

---
+++ Tracker/Modules/Category/TrackerCategoryViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  TrackerCategoryViewModel.swift
+//  Tracker
+//
+//  Created by Roman Yaschenkov on 22.10.2025.
+//
+
+final class TrackerCategoryViewModel {
+    // MARK: - Definition
+    private var categories: [TrackerCategory] = []
+}
