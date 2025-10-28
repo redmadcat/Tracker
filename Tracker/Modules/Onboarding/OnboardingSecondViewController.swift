28798faa7f61bbecc4b15f76a26e7c0c64697650
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-10-28 21:44:18 +0300

    fix: onboarding background image issue
    Fix onboarding background image issue based on code review.

--- Tracker/Modules/Onboarding/OnboardingSecondViewController.swift
+++ Tracker/Modules/Onboarding/OnboardingSecondViewController.swift
@@ -8,23 +8,66 @@
 import UIKit
 
 final class OnboardingSecondViewController: OnboardingViewController {
+    // MARK: - Definition
+    private lazy var doneButton = UIButton()
+    private lazy var label = UILabel()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "OnBoardingImage2"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        configureLayout()
     }
     
     // MARK: - Private func
     private func configureUI() {
-        // imageView
-        imageView.image = UIImage(named: "OnboardingImage2")
+        // doneButton
+        doneButton.setTitle("Вот это технологии!", for: .normal)
+        doneButton.setTitleColor(.ypWhite, for: .normal)
+        doneButton.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        doneButton.backgroundColor = .ypBlack
+        doneButton.layer.cornerRadius = 16
+        doneButton.addTarget(self, action: #selector(doneButtonTapped), for: .touchUpInside)
+        doneButton.translatesAutoresizingMaskIntoConstraints = false
         
         // label
+        label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = """
             Даже если это
             не литры воды и йога
             """
         label.textAlignment = .center
+        
+        // hierarchy
+        view.addSubview(imageView)
+        view.addSubview(label)
+        view.addSubview(doneButton)
+    }
+    
+    private func configureLayout() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            label.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -270),
+                        
+            doneButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            doneButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            doneButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            doneButton.heightAnchor.constraint(equalToConstant: 60)
+        ])
     }
 }
 
